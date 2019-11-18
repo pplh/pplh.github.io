3731d4586a9a71881b8b8e8eaeb7a9d396390651
pplh <57901629+pplh@users.noreply.github.com>
2019-11-18 21:22:06 +0900

    Add files via upload

---
+++ barcode.js
@@ -0,0 +1,54 @@
+function reset(){
+  location.reload();
+}
+
+function makeCode(){
+  checkD();
+  $('#barcode1').barcode(inpCd.bar1.value,'code128');
+  $('#barcode2').barcode(inpCd.bar2.value,'code128');
+}
+
+function checkD(){
+  var bar1=Number(inpCd.bar1.value)+370000000000000;
+  var bar2=Number(inpCd.bar2.value)+370000000000000;
+  inpCd.bar1.value=String(bar1)+String(calcBar(bar1));
+  inpCd.bar2.value=String(bar2)+String(calcBar(bar2));
+}
+
+function calcBar(bar){
+  var a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,num;
+  bar=String(bar);
+  a=bar.substr(0,1)*1;
+    if(String(a).length==2){a=String(a);a=Number(a.substr(0,1))+Number(a.substr(1,1))};
+  b=bar.substr(1,1)*2;
+    if(String(b).length==2){b=String(b);b=Number(b.substr(0,1))+Number(b.substr(1,1))};
+  c=bar.substr(2,1)*1;
+    if(String(c).length==2){c=String(c);c=Number(c.substr(0,1))+Number(c.substr(1,1))};
+  d=bar.substr(3,1)*2;
+    if(String(d).length==2){d=String(d);d=Number(d.substr(0,1))+Number(d.substr(1,1))};
+  e=bar.substr(4,1)*1;
+    if(String(e).length==2){e=String(e);e=Number(e.substr(0,1))+Number(e.substr(1,1))};
+  f=bar.substr(5,1)*2;
+    if(String(f).length==2){f=String(f);f=Number(f.substr(0,1))+Number(f.substr(1,1))};
+  g=bar.substr(6,1)*1;
+    if(String(g).length==2){g=String(g);g=Number(g.substr(0,1))+Number(g.substr(1,1))};
+  h=bar.substr(7,1)*2;
+    if(String(h).length==2){h=String(h);h=Number(h.substr(0,1))+Number(h.substr(1,1))};
+  i=bar.substr(8,1)*1;
+    if(String(i).length==2){i=String(i);i=Number(i.substr(0,1))+Number(i.substr(1,1))};
+  j=bar.substr(9,1)*2;
+    if(String(j).length==2){j=String(j);j=Number(j.substr(0,1))+Number(j.substr(1,1))};
+  k=bar.substr(10,1)*1;
+    if(String(k).length==2){k=String(k);k=Number(k.substr(0,1))+Number(k.substr(1,1))};
+  l=bar.substr(11,1)*2;
+    if(String(l).length==2){l=String(l);l=Number(l.substr(0,1))+Number(l.substr(1,1))};
+  m=bar.substr(12,1)*1;
+    if(String(m).length==2){m=String(m);m=Number(m.substr(0,1))+Number(m.substr(1,1))};
+  n=bar.substr(13,1)*2;
+    if(String(n).length==2){n=String(n);n=Number(n.substr(0,1))+Number(n.substr(1,1))};
+  o=bar.substr(14,1)*1;
+    if(String(o).length==2){o=String(o);o=Number(o.substr(0,1))+Number(o.substr(1,1))};
+  num=a+b+c+d+e+f+g+h+i+j+k+l+m+n+o;
+  num=(num%10)==0?0:10-(num%10);
+  return num;
+}
---
+++ check.js
@@ -0,0 +1,23 @@
+function check(){
+  var bar1=inpCd.bar1.value+370000000000000;
+  var bar2=inpCd.bar2.value+370000000000000;
+}
+
+function calc(bar){
+  var a,b,c,d,e,f,g,h,i,j,j,l,m,n,o;
+  a=bar.substr(1,1)*1;
+  b=bar.substr(1,1)*2;
+  c=bar.substr(1,1)*1;
+  d=bar.substr(1,1)*2;
+  e=bar.substr(1,1)*1;
+  f=bar.substr(1,1)*2;
+  g=bar.substr(1,1)*1;
+  h=bar.substr(1,1)*2;
+  i=bar.substr(1,1)*1;
+  j=bar.substr(1,1)*2;
+  k=bar.substr(1,1)*1;
+  l=bar.substr(1,1)*2;
+  m=bar.substr(1,1)*1;
+  n=bar.substr(1,1)*2;
+  o=bar.substr(1,1)*1;
+}
